a2f7f625f80058972f50cc60a6e417fb1a27f48c
Jianqi Pan <jannchie@gmail.com>
2025-06-05 21:18:42 +0900

    refactor(tags-benchmark)!: remove underscore replacement in tag outputs && improve type hints and paths in benchmark

--- tests/benchmark_tagger.py
+++ tests/benchmark_tagger.py
@@ -1,4 +1,6 @@
 import os
+from collections.abc import Sequence
+from pathlib import Path
 from typing import Any
 
 import pytest
@@ -7,12 +9,12 @@ from PIL import Image
 from wdtagger import Tagger
 
 tagger = Tagger()
-image_dir = "./tests/images/"
-image_paths = [os.path.join(image_dir, image) for image in os.listdir(image_dir)] * 16
+image_dir = Path("./tests/images/")
+image_paths = [(image_dir / image) for image in os.listdir(image_dir)] * 16
 images = [Image.open(image_path) for image_path in image_paths]
 
 
-def tag_in_batch(images: Any, batch: Any = 1) -> None:
+def tag_in_batch(images: Sequence[Image.Image], batch: int = 1) -> None:
     for i in range(0, len(images), batch):
         tagger.tag(images[i : i + batch])
 
@@ -24,7 +26,7 @@ def tag_in_batch(images: Any, batch: Any = 1) -> None:
     disable_gc=True,
 )
 @pytest.mark.parametrize("batch", [1, 2, 4, 8, 16])
-def test_tagger_benchmark(benchmark: Any, batch: Any) -> None:
+def test_tagger_benchmark(benchmark: Any, batch: int) -> None:  # noqa: ANN401
     # warmup
     tag_in_batch(images[:1])
     benchmark.pedantic(tag_in_batch, args=(images, batch), iterations=1, rounds=10)
